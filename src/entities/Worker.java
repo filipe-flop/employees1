dea394867ed67209fa90ae44bc18a51763815f91
Filipe Alan da Silva de Oliveira <fsoliveira@teconsuape.inet>
2022-07-19 15:28:03 -0300

    created constructors, getters and setters for Worker class

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -2,6 +2,7 @@ package entities;
 
 import entities.enums.WorkerLevel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Worker {
@@ -11,5 +12,56 @@ public class Worker {
 
     //associations
     private Department department;
-    private List<HourContract> contracts;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(List<HourContract> contracts) {
+        this.contracts = contracts;
+    }
+
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker(){
+    }
+
+    public Worker(String name, Double baseSalary, WorkerLevel level, Department department) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+        this.level = level;
+        this.department = department;
+    }
 }
